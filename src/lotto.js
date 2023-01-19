3cd71e42c07e74c69854de21860ce80c79ed1fcc
Grzegorz Wozniak <grubysweter@gmail.com>
2023-01-19 11:46:21 +0100

    Added write in localStorage and verify if exist

--- src/lotto.js
+++ src/lotto.js
@@ -1,36 +1,41 @@
 let birthDay = parseInt(prompt("Your birthday: ddmmyy", ""));
-let numRes = [];
+if (localStorage.getItem(birthDay) === null) {
+  let numRes = [];
 
-for (let i = 0; i < 300377; i++) {
-  let numbers = [];
+  for (let i = 0; i < 300377; i++) {
+    let numbers = [];
 
-  for (let j = 0; j < 6; j++) {
-    let number = Math.floor(Math.random() * 49) + 1;
+    for (let j = 0; j < 6; j++) {
+      let number = Math.floor(Math.random() * 49) + 1;
 
-    if (!numbers.includes(number)) {
-      numbers.push(number);
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      } else {
+        j--;
+      }
+    }
+
+    numRes.push(...numbers);
+  }
+
+  const tabNum = {};
+
+  for (let i = 0; i < numRes.length; i++) {
+    const num = numRes[i];
+    if (tabNum[num]) {
+      tabNum[num]++;
     } else {
-      j--;
+      tabNum[num] = 1;
     }
   }
+  const mostFrequentNumbers = Object.keys(tabNum)
+    .sort((a, b) => tabNum[b] - tabNum[a])
+    .slice(0, 6);
+  const tabFin = [];
+  tabFin.push(mostFrequentNumbers);
 
-  numRes.push(...numbers);
+  localStorage.setItem(birthDay, ...tabFin);
+  document.write("Your luckie numbers: ", ...tabFin);
+} else {
+  document.write("Your luckie numbers: ", ...localStorage.getItem(birthDay));
 }
-
-const tabNum = {};
-
-for (let i = 0; i < numRes.length; i++) {
-  const num = numRes[i];
-  if (tabNum[num]) {
-    tabNum[num]++;
-  } else {
-    tabNum[num] = 1;
-  }
-}
-const mostFrequentNumbers = Object.keys(tabNum)
-  .sort((a, b) => tabNum[b] - tabNum[a])
-  .slice(0, 6);
-const tabFin = [];
-tabFin.push(mostFrequentNumbers);
-
-document.write("Your luckie numbers: ", ...tabFin);
